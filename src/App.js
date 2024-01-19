e9a09ed917e921df5941d19feb6a2bd7cd67c219
Brian Kimathi <kimathibrian71@gmail.com>
2024-01-20 00:25:50 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import ContactForm from "./components/ContactForm";
 import Dropdown from "./components/Dropdown";
 import Features from "./components/Features";
 import FilterComponent from "./components/FilterComponent";
@@ -10,15 +11,17 @@ import ParentComponent from "./components/Parent";
 import ProductView from "./components/ProductView";
 import Products from "./components/Products";
 import Register from "./components/Register";
+import SearchBar from "./components/SearchBar";
 import ServiceCards from "./components/ServiceCard";
 import ViewMore from "./components/ViewMore";
 import ProductsPage from "./pages/ProductsPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <ProductsPage />
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <div class="relative max-w-sm mx-auto mt-20">
+      <input
+        class="w-full py-2 px-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        type="search"
+        placeholder="Search"
+      />
+      <button class="absolute inset-y-0 right-0 flex items-center px-4 text-gray-700 bg-gray-100 border border-gray-300 rounded-r-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
+        <svg
+          class="h-5 w-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M14.795 13.408l5.204 5.204a1 1 0 01-1.414 1.414l-5.204-5.204a7.5 7.5 0 111.414-1.414zM8.5 14A5.5 5.5 0 103 8.5 5.506 5.506 0 008.5 14z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+export default SearchBar;
